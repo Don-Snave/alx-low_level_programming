53d133a3c5795d01ca117efae777289feb91822d
Don-Snave <chionyenanu@gmail.com>
2023-06-20 14:48:50 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,9 +1,12 @@
+/*
+* File: 101-natural.c
+* Auth: Evans
+*/
 #include <stdio.h>
 
-
-/*
-* main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
-*        separated by a comma followed by a space.
+/**
+* main - Lists all the natural numbers below 1024 (excluded)
+*        that are multiples of 3 or 5.
 *
 * Return: Always 0.
 */
